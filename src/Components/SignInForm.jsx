23781ba8f64b83fb84dd7727f2248b800a7d5654
JonaC <jc2941211@gmail.com>
2025-04-22 20:08:02 -0500

    added forgot password and revised original styling

--- src/Components/SignInForm.jsx
+++ src/Components/SignInForm.jsx
@@ -7,38 +7,34 @@ const SignInForm = () => {
             <div className='form'>
                 <div className='form-title'>Sign in</div>
                 <div className='form-subtitle'>Enter your details to continue</div>
-                <div className='label-container-1'>
-                    <label 
-                    className='email-label'
-                    htmlFor="Email">Email</label>
+                <div className='container'>
+                    <label
+                        className='label'
+                        htmlFor="Email">Email</label>
                     <input
-                    className='input-1'
+                        className='input'
                         type="Email"
                         id='Email'
                         placeholder='john@email.com'
                     />
                 </div>
-                <div className='label-container-1'>
-                    <label 
-                    className='password-label'
-                    htmlFor="Password">Password</label>
+                <div className='container'>
+                    <label
+                        className='label'
+                        htmlFor="Password">Password</label>
                     <input
-                    className='input-2'
+                        className='input'
                         type="Password"
                         id='Password'
                     />
-                    </div>
-                    <div>
-                        <h3>Forgot Password</h3>
-                    </div>
-                    <div>
-                        <a href="">Forgot Password</a>
-                    </div>
-                    <button
-                        type='submmit'
-                        className='sign-in'>Sign In </button>
-
+                </div>
+                <button className='forgot-password'>Forgot password?</button>
+                <button
+                    type='submmit'
+                    className='btn'>Sign In
+                </button>
             </div>
+
         </div>
     )
 }
