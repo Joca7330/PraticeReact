fd3ff3e76b72d8a3153aa7847661ab8918f16112
JonaC <jc2941211@gmail.com>
2025-05-01 03:13:03 -0500

    implemented toggle between SignIn and SignUp forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 
-import {React,useState} from 'react'
+import { React, useState } from 'react'
 import SignInForm from './forms/SignInForm'
 import SignUpForm from './forms/SignUpForm'
 import DetailsToggle from './conditional-rendering/AuthForms'
 import ToggleLogin from './conditional-rendering/ToggleLogin'
 import OneToggleLogin from './conditional-rendering/OneToggleLogin'
 import Child from './props/Child'
+import { useRef } from 'react'
 
 
 
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  console.log('state:', isLoggedIn);
-  
   const handleLogIn = () => {
     setIsLoggedIn(true);
   }
-const handleLogOut = () => {
-  setIsLoggedIn(false);
-}
+  const handleLogOut = () => {
+    setIsLoggedIn(false);
+  }
 
   return (
     <div className='backdrop'>
       <div className='form-2-container'>
         {!isLoggedIn ?
-        <div>
-        <SignInForm onLogin={handleLogIn} />
-        </div>
-         : <SignUpForm onLogout={handleLogOut} />}
+          <div>
+            <SignInForm 
+            isLoggedIn={isLoggedIn}
+            onLogin={handleLogIn} />
+          </div>
+          : <SignUpForm onLogout={handleLogOut} />}
+
       </div>
     </div>
   )
