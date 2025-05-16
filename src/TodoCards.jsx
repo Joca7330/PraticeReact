90506acf224ff9a58907b21eeea19495cbe5ffda
JonaC <jc2941211@gmail.com>
2025-05-15 20:40:23 -0500

    added div structure for styling todo cards

--- src/TodoCards.jsx
+++ src/TodoCards.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { data } from 'react-router-dom';
+import './TodoCards.css'
 
 const TodoCards = () => {
   const [showToDo, setShowTodo] = useState([]);
@@ -8,10 +9,13 @@ const TodoCards = () => {
   useEffect(() => {
     const fetchTodos = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
         const data = await response.json();
-        console.log('data', data);
-        setShowTodo(data); // Update state with the fetched data
+        // console.log('data', data);
+        const limitedData = data.slice(0, 9)
+        console.log('limited data', limitedData);
+
+        setShowTodo(limitedData); // Update state with the fetched data
       } catch (error) {
         console.log('Failed to fetch todos', error);
       }
@@ -19,11 +23,23 @@ const TodoCards = () => {
     fetchTodos(); // Call the async function
   }, []);
   return (
-    <div>
-      {showToDo.map((todo) => (
-        todo.title
-      ))}
-      <div>
+    <div className='todo-container'>
+      <div className='container'>
+        {showToDo.map((todo) => (
+          <div className='todo'>
+            <div className='title'>
+              {todo.title}
+            </div>
+            <div className='id'>
+              {todo.id}
+            </div>
+            <div className='completed'>
+              {/* {todo.completed} */}
+            </div>
+          </div>
+        ))}
+        <div>
+        </div>
       </div>
     </div>
   )
